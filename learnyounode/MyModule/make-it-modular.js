7165a63e3a0693a199c2c9b2948ee34c067149c4
wwwbrunagon <wwwbrunagon@gmail.com>
2023-04-25 10:07:23 -0300

    learnyounode/MyModule/mymodule.js

--- learnyounode/MyModule/make-it-modular.js
+++ learnyounode/MyModule/make-it-modular.js
@@ -1,19 +1,20 @@
-'use strict'
+'use strict';
+
 const log = console.log.bind(console);
 
-const { filterDir } = require('./mymodule');
+const filterDir = require('./mymodule');
 
 const getParams = processArgv => processArgv.slice(2);
 
 const [dir, ext] = getParams(process.argv);
 
-
 const print = (err, files) => {
-    if (err) {
-        log('Gave Wood');
-        return;
-    }
-    log(files);
+  if (err) {
+    log('Gave wood!');
+    return;
+  }
+
+  log(files.join('\n'));
 };
 
 filterDir(dir, ext, print);
--- learnyounode/MyModule/mymodule.js
+++ learnyounode/MyModule/mymodule.js
@@ -1,24 +1,88 @@
-'use strict'
-const { readdir } = require('node:fs');
+'use strict';
+
+const fs = require('node:fs');
 
 /**
- * 
- * @param {string} dir 
- * @param {string} ext 
- * @param {(Error, string[]) => undefined } cb 
+ * List files in `dir` filtered by `ext`.
+ *
+ * @param {string} dir
+ * @param {string} dir
+ * @param {(Error, string[]) => undefined} cb
  */
-const filterDir = (dir, ext, cb) => {
-    const re = new RegExp(`\\.${ext}`)
-       
-    readdir(dir, (err, files) => {
-        if (err) return cb(err);
-    
-        const filteredFiles = files.filter(file => {
-            return re.test(file);
-        });
+function filteredReadDir(dir, ext, callback) {
+  const re = new RegExp(`\\.${ext}$`);
 
-        cb(null, filteredFiles);
-    });
+  fs.readdir(dir, function (err, files) {
+    if (err) { return callback(err); }
+
+    const filteredFiles = files.filter(file => re.test(file));
+
+    callback(null, filteredFiles);
+  });
 };
 
-module.exports = { filterDir }
+module.exports = filteredReadDir;
+
+/*
+
+UMD
+AMD
+
+CommonJS (Nod.js)
+ES Modules (ECMAScript spec)
+
+Defaults exports and named exports.
+
+CommonJS default export:
+
+    module.exports = myFunction;
+
+    module.exports = myObj;
+
+    module.exports = myArr;
+
+CommonJS default import:
+
+    const myFunc = require('./some-module');
+    const anyNameIWant = require('./some-module');
+
+CommonJS named export:
+
+    module.exports = {
+      myFunction1,
+      someClass,
+      someObj,
+    };
+
+CommonJS named import (require):
+
+    const { myFunction1, someClass } = require('./my-module');
+
+--------------------------------------------------------------------
+
+ES Modules default export:
+
+    export default myFunction;
+
+    export default myObj;
+
+    export derfault myArr;
+
+ES Modules default import:
+
+    import myFunc from './some-module';
+    import anyNameIWant from './some-module';
+
+ES Modules named export:
+
+    export {
+      myFunction1,
+      someClass,
+      someObj,
+    };
+
+ES Modules named import:
+
+    import { myFunction1, someClass } from './my-module';
+
+*/
