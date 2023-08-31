faf0ced1ba2cf8c312363bdc9539338b5162bc18
wwwbrunagon <wwwbrunagon@gmail.com>
2023-08-31 17:42:09 -0300

    WIP

--- Promise/index-v2.js
+++
@@ -1,8 +0,0 @@
-const log = console.log.bind(console);
-
-async function getData() {
-  const result = await fetch('https://jsonplaceholder.typicode.com/todos/1')
-    .then((res) => res.json());
-  return log(result);
-}
-getData();
--- Promise/index.js
+++
@@ -1,8 +0,0 @@
-const log = console.log.bind(console);
-
-async function getData() {
-  const result = await fetch('https://jsonplaceholder.typicode.com/todos/1');
-  const convertedResult = await result.json();
-  return log(convertedResult);
-}
-getData();
